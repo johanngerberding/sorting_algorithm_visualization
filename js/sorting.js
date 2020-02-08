7453d9e3dd93349848c29ea96704c3dbfe8875e3
Johann Gerberding <johanngerberding@Johanns-MBP.multi.box>
2020-02-08 19:12:25 +0100

    insertion sort works

--- js/sorting.js
+++ js/sorting.js
@@ -1,11 +1,21 @@
 var arr = [];
 var alg = $('#alg :selected').text();
+var delay = 250;
 
-console.log($('#alg').children());
+$('#interrupt').on('click', () => {
+    location.reload();
+});
 
-$('#display').on("click", function() {
+$('#header').css('height', ($(window).innerHeight() * 0.15).toString() + "px");
+$('#visualization').css('height', ($(window).innerHeight() * 0.70).toString() + "px");
+
+$('#display').on("click", () => {
     alg = $('#alg :selected').text();
-    console.log(alg);
+    if(Number.isNaN(parseInt($('#delay').val()))) {
+        delay = 250;
+    } else {
+        delay = parseInt($('#delay').val());
+    }
     // String to Int, if it fails NaN is returned
     var number = parseInt($('#num').val());
     if (Number.isNaN(number)) {
@@ -25,11 +35,15 @@ $('#display').on("click", function() {
 });
 
 
-$('#sort').on("click", function() {
-    console.log("old array: ", arr);
-    arr = insertionSort(arr);
-    console.log("new array: ", arr);
-    sortArray(arr, alg);
+$('#sort').on("click", () => {
+    // disable buttons while sorting
+    $('#display').prop('disabled', true);
+    $('#sort').prop('disabled', true);
+    $('#alg').prop('disabled', true);
+    $('#num').prop('disabled', true);
+    $('#delay').prop('disabled', true);
+
+    arr = sortArray(arr, alg, delay);
 });
 
 function randomNumbers(n) {
@@ -52,34 +66,53 @@ function visualizeArray(arr) {
         var displayWidth = elWidth * 0.9;
         var displayMargin = elWidth * 0.05;
         $("<div/>")
-            .attr('class','number')
+            .attr({'class': 'number', 'id': 'num' + i})
             .css({'width': displayWidth, 'height': displayHeight, 'margin': displayMargin})
             .html(arr[i])
             .appendTo('#visualization');
     }
 }
 
-function sortArray(arr, alg) {
+function sortArray(arr, alg, delay) {
     // This function initializes the sorting process
     if(alg == "Insertion Sort") {
-        arr = insertionSort(arr);
+        arr = insertionSort(arr, delay);
     } else {
         console.log("This algorithm isn't implemented yet.");
     }
-    visualizeArray(arr);
+    //visualizeArray(arr);
     return arr;
 }
 
-function insertionSort(arr) {
+async function insertionSort(arr, delay) {
+    
     // Logic of the Insertion Sort Algorithm
     for(var i = 0; i < arr.length; i++) {
         var el = arr[i];
+        var element = $('#num' + i);
+        element.css('background-color', 'steelblue');
+        await sleep(delay);
         var j = i;
         while(j > 0 && arr[j-1] > el) {
+            var checkElement = $('#num' + (j-1)).css('background-color', 'yellow');
             arr[j] = arr[j-1];
             j--;
+            await sleep(delay);
+            checkElement.css('background-color', 'lightgray');   
         }
+        $('#num' + j).css('background-color', 'lightgreen');
         arr[j] = el;
+        await sleep(delay);
+        visualizeArray(arr);
     }
+    $('#display').prop('disabled', false);
+    $('#sort').prop('disabled', false);
+    $('#alg').prop('disabled', false);
+    $('#num').prop('disabled', false);
+    $('#delay').prop('disabled', false);
     return arr;
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
